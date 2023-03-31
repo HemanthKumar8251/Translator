73d5557267bd11b7976c16e0e43b14e9296acb2b
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2023-03-31 15:22:49 +0530

    addded s2t code

--- Translator/Translate/urls.py
+++ Translator/Translate/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('T2T', views.T2T,name='T2T'),
-    path('T2TR', views.T2TR,name='T2TR'),
     path('S2T', views.S2T,name='S2T')
 ]
--- Translator/Translate/views.py
+++ Translator/Translate/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from googletrans import Translator
 from django.http import HttpResponse
+import speech_recognition as speech  
 
 
 # Create your views here.
 def T2T(request):
-    return render(request,'Translate/t2t.html')
-def T2TR(request):
+    if(request.method == 'GET'):
+        return render(request,'Translate/t2t.html')
     if(request.method == 'POST'): 
-        print('post')  
+        print('post') 
         text = request.POST['text']
         print(text)
         print(type(text))
@@ -16,13 +17,36 @@ def T2TR(request):
         de = request.POST['de']
         print(sr,de)
         translator = Translator()
-        translated = translator.translate(text, sr, de)
+        translated = translator.translate(text,src= sr, dest =  de)
         result = translated.text
         print(result)
-        return render(request,'Translate/t2tR.html',{'result':result})
-    if(request.method == 'GET'):
-        return HttpResponse("JHgdjhg")
+        return render(request,'Translate/t2t.html',{'result':result})
+
+    
 def S2T(request):
-    pass
+     if(request.method == 'GET'):
+        return render(request,'Translate/s2t.html')
+     if(request.method == 'POST'): 
+        print('post') 
+        sr = request.POST['sr']
+        de = request.POST['de']
+        print(sr,de)
+        
+        r = speech.Recognizer()
+        with speech.Microphone() as s:
+          print("Speak now...")
+        audio = r.listen(s)
+        try:
+            text = r.recognize_google(audio, language=sr)
+
+            print(f"you have said {text}")
+            translator = Translator()
+            translated = translator.translate(text,src= sr, dest =  de)
+            result = translated.text
+        except Exception as e:
+            result = "Error: {str(e)}"
+        return render(request, 'Translate/s2t.html', {
+            'result' :result
+        })
 def home(request): 
     return render(request,'Translate/index.html')
